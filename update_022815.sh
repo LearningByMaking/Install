a797c788bfda8d94c4193e714d89598b7843d52d
Kevin John <kj@universe.sonoma.edu>
2015-02-27 11:30:04 -0800

    added disable touchpad and USB rules files to make install complete

--- update_022815.sh
+++ update_022815.sh
@@ -7,6 +7,15 @@ sudo modprobe rtl8723be
 
 #move over new i3 bin
 sudo unzip i3.zip -d /
+
+#move over nwe bash rules
 sudo cp .bashrc ~/
 
+#move over USB rules
+sudo cp 77-ulogo.rules /etc/udev/rules.d/
+
+#move over DisableTouchpad
+sudo mkdir ~/Documents/Scripts
+sudo cp DisableTouchpad.sh ~/Documents/Scripts
+sudo cp DisableTouchpad.sh.desktop ~/.config/autostart
 $SHELL
